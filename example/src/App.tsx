c179a0879fc59f383dec097804187a545913fdfd
Julien <jbertazzolambert@gmail.com>
2021-06-19 00:15:06 -0500

    initial project

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-list-extender'
+import 'react-list-extender/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
